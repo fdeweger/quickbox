38efc3032ae50d256d305aa146006711ed6ba3d8
Frank de Weger <frankdeweger@gmail.com>
2015-02-09 10:57:14 +0100

    Enable custom php.ini (you really want error_reporting on E_ALL for development)

--- support/puppet/manifests/quickbox.pp
+++ support/puppet/manifests/quickbox.pp
@@ -21,7 +21,7 @@ file { '/etc/motd' :
 }
 
 # Automagically goto /vagrant when using vagrant ssh
-exec { "cdvagrant" :
+exec { "cd-vagrant" :
     command => "echo 'cd /vagrant' >> /home/vagrant/.bashrc",
     unless => "grep -c 'cd /vagrant' /home/vagrant/.bashrc"
 }
@@ -57,9 +57,7 @@ class quickbox::apache {
 }
 
 class quickbox::php {
-    # Bog standard php5 install from puppetlabs-apache. No fancy shmancy xdebug, and a factory default php.ini (you'll
-    # probably need to tune memory_limit).
-
+    # Bog standard php5 install from puppetlabs-apache. No fancy shmancy xdebug.
     # This won't work if you want CLI php only, or want to use nginx / lighttpd, since it also depends on mpm_module
     # set to prefork in the apache module (you can copy the class include though, that is untested but should work)
     class {'::apache::mod::php' : }
@@ -71,6 +69,21 @@ class quickbox::php {
     package { 'php5-curl' :
         ensure => installed,
         require => Class['::apache::mod::php']
+    }
+
+    # Use custom php.ini for apache
+    file { '/etc/php5/apache2/php.ini' :
+        source => '/vagrant/support/puppet/files/php.ini',
+        ensure => present,
+        require => Class['::apache::mod::php'],
+        notify  => Service['apache2'],
+    }
+
+    # Use custom php.ini for CLI
+    file { '/etc/php5/cli/php.ini' :
+        source => '/vagrant/support/puppet/files/php.ini',
+        ensure => present,
+        require => Class['::apache::mod::php'],
     }
 }
 
